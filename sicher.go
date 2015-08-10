32286e746db897401044ddece19a4e73eaf7cec4
Tomohiko Himura <eiel.hal@gmail.com>
2015-08-10 23:30:56 +0900

    issue #1: 複数URLに対応

--- sicher.go
+++ sicher.go
@@ -13,6 +13,7 @@ import (
 func init() {
     http.HandleFunc("/", handler)
     http.HandleFunc("/checks", checksHandler)
+    http.HandleFunc("/hping", hpingHandler)
     http.HandleFunc("/notification/slack", slackHandler)
 }
 
@@ -22,14 +23,37 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func checksHandler(w http.ResponseWriter, r *http.Request) {
     c := appengine.NewContext(r)
+    myurls := []string{
+      "http://eiel.info/",
+      "http://parkmap.eiel.info/",
+    }
+    for _, value := range myurls {
+       t := taskqueue.NewPOSTTask("/hping",
+         map[string][]string{"url": {value}})
+       if _, err := taskqueue.Add(c, t, ""); err != nil {
+           http.Error(w, err.Error(), http.StatusInternalServerError)
+           return
+       }
+    }
+}
+
+func hpingHandler(w http.ResponseWriter, r *http.Request) {
+    url := r.FormValue("url")
+
+    c := appengine.NewContext(r)
     client := urlfetch.Client(c)
-    myurl := "http://eiel.info/"
-    resp, err := client.Head(myurl)
+
+    resp, err := client.Head(url)
     if err != nil {
         c.Debugf("Fail",)
        fmt.Fprint(w, err.Error())
        // TODO datastroeに記録する
-       // TODO 通知する
+       t := taskqueue.NewPOSTTask("/notification/slack",
+       map[string][]string{"url": {url}})
+       if _, err := taskqueue.Add(c, t, ""); err != nil {
+           http.Error(w, err.Error(), http.StatusInternalServerError)
+           return
+       }
        // TODO 状態を失敗に変更
     } else {
         c.Debugf("Success",)
@@ -38,12 +62,6 @@ func checksHandler(w http.ResponseWriter, r *http.Request) {
        // TODO 高度なチェックを起動
        // TODO 状態が失敗なら成功になったことを通知
        // TODO 状態を成功に変更
-    }
-    t := taskqueue.NewPOSTTask("/notification/slack",
-      map[string][]string{"url": {myurl}})
-    if _, err := taskqueue.Add(c, t, ""); err != nil {
-       http.Error(w, err.Error(), http.StatusInternalServerError)
-       return
     }
 }
 
